c46ec844e332682e83b03d9e6266dfd7cad14780
lhuddlesto <lhuddlesto@gannett.com>
2019-01-10 09:46:27 -0500

    removed useless imports

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles.scss';
 import App from './App';
-import Tweet from './Tweet'
-import Next from './Next'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
