5ff12795437691c9c8550e017afda5d3c5fb2f8c
lhuddlesto <lhuddlesto@gannett.com>
2019-01-12 18:50:26 -0500

    Updates

---
+++ precache-manifest.c101bad9cae6807a911c664442e0c675.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "de7c4a51e89805994aa8",
+    "url": "/static/css/main.7dc4f1bf.chunk.css"
+  },
+  {
+    "revision": "de7c4a51e89805994aa8",
+    "url": "/static/js/main.de7c4a51.chunk.js"
+  },
+  {
+    "revision": "a6c6aed7afd41ac6410b",
+    "url": "/static/js/1.a6c6aed7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5c336740d0354507bcd61ffbf7d9d41a",
+    "url": "/static/media/tatsuhiro_satou.5c336740.jpg"
+  },
+  {
+    "revision": "1e7b97caa7abcc1b733aee3723baf94e",
+    "url": "/static/media/edward_elric.1e7b97ca.jpg"
+  },
+  {
+    "revision": "6171eb13afeb1dd90ee1db05c364ef9c",
+    "url": "/static/media/naruto_uzumaki.6171eb13.jpg"
+  },
+  {
+    "revision": "c80a5dbf31d3ac97427b4af4fd9343bc",
+    "url": "/static/media/violet_evergarden.c80a5dbf.jpg"
+  },
+  {
+    "revision": "d46c83f1f23dcbe596a2dc09c84dcc51",
+    "url": "/static/media/eren_jaeger.d46c83f1.jpg"
+  },
+  {
+    "revision": "2fb66cb0fc2c6cf1a31f5510cdeb2859",
+    "url": "/static/media/mikasa_ackermann.2fb66cb0.jpg"
+  },
+  {
+    "revision": "5f8c4b57dd38ad460a6ac75d8e955023",
+    "url": "/static/media/katsuki_bakugo.5f8c4b57.jpg"
+  },
+  {
+    "revision": "7de5117bfa6e3f71a0b62bbaf4840604",
+    "url": "/static/media/misaki_nakahara.7de5117b.jpg"
+  },
+  {
+    "revision": "1a3738d3a2cab4b77588b301a71d64e7",
+    "url": "/static/media/shoto_todoroki.1a3738d3.jpg"
+  },
+  {
+    "revision": "08669611397bb8386867556a2dc2a37c",
+    "url": "/static/media/hEFk1Sf.08669611.jpg"
+  },
+  {
+    "revision": "80e461a507ee582ff590e55f76704744",
+    "url": "/static/media/izuku_midoriya.80e461a5.png"
+  },
+  {
+    "revision": "5fba7500cf4225c4f3beaf3f5f3bee42",
+    "url": "/static/media/all_might.5fba7500.png"
+  },
+  {
+    "revision": "2467afef9b88aadefd4975b0d5d9c137",
+    "url": "/static/media/kakashi_hatake.2467afef.png"
+  },
+  {
+    "revision": "e0013553131770f4de4e21ff3e31e7c3",
+    "url": "/static/media/twitter.e0013553.svg"
+  },
+  {
+    "revision": "1de1e6077ad49300e492f7a3729ec5a0",
+    "url": "/static/media/arrow.1de1e607.svg"
+  },
+  {
+    "revision": "4fdfd1baa70b6837d2d4c6074bf72c6b",
+    "url": "/static/media/quote.4fdfd1ba.svg"
+  },
+  {
+    "revision": "efbd31d01f6495958c61768c1dfebcd7",
+    "url": "/index.html"
+  }
+];
